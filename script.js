ddc968cbb9aff420cb8762b32f4d574c87807874
Lucacia <lucaciarovaniaina1003@gmail.com>
2024-11-18 15:08:12 +0300

    inital commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function openNav(){
+    document.getElementById("mySidebar").style.width="100vw";
+    document.querySelector("body").style.overflow= "hidden"
+
+}
+
+function closeNav(){
+    document.getElementById("mySidebar").style.width="0";
+    document.querySelector("body").style.overflow= "auto"
+
+}
